66fe1ef75eddc80bc26d5f33ac3f983def362751
sukhmani <sukhmani@bellevuecollege.edu>
2018-10-10 19:31:29 -0700

    button

--- ListButton/Index.js
+++ ListButton/Index.js
@@ -1,10 +1,17 @@
-window.onload = () => {
+const setTextButton = document.getElementById('set-text');
+const userInput = document.getElementById('list-data');
+const listButton = document.getElementById('add-to-list');
 
-    const userInput = document.getElementById('list-data');
+window.onload = () => {
+    const userInput = document.getElementById('list-data');    
     const setTextButton = document.getElementById('set-text');
 
-    setTextButton.onclick = () => {
-        userInput.value = "a"
-    }
+    listButton.onclick = () => {
+    const myList = document.getElementById('my-list');
+    elfCode.appendToList(myList, userInput.value);
+}
 
+    setTextButton.onclick = () => {
+       userInput.value = 'The first item for my list';
+    }
 }
