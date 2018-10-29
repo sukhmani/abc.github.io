124da6367c07e33eb96c4cd89569395e934a71e4
sukhmani <sukhmani@bellevuecollege.edu>
2018-10-29 12:19:47 -0700

    javascript-test

--- javascript-tests/week07-day1.js
+++ javascript-tests/week07-day1.js
@@ -1,4 +1,4 @@
-const num =3;
+let num =3;
 const numberString = '3';
 const rain = true;
 
@@ -13,3 +13,39 @@ else{
     console.log('walk');
 }
 
+num = 5;
+console.log('modulus test', num % 2);
+console.log('modulus test', (num % 2)=== 0);
+
+function evenOrOdd(value) {
+    if((value % 2)=== 0) {
+        console.log('The number '+ value + ' is even');
+    }
+    else {
+        console.log('The number '+ value + ' is odd');
+    }
+}
+
+evenOrOdd(4);
+evenOrOdd(15);
+
+
+if((num % 2)=== 0) {
+    console.log('The number '+ num+ ' is even');
+}
+else {
+    console.log('The number '+ num + ' is odd');
+}
+num = 4;
+console.log('modulus test', (num % 2)=== 0);
+
+var app = {
+    isEven: function(input) {
+        if (input % 2 === 0) {
+    console.log('Your input of'+ input + 'even');
+        }else {
+    console.log('Your input of'+ input + 'odd');
+}
+console.log('modulus test', (num % 2)=== 0);
+}
+};
