a62de43a543dd0768f69feb476fb69a24a372027
sukhmani <sukhmani@bellevuecollege.edu>
2018-10-29 11:28:21 -0700

    operators- assignment & comparison

---
+++ javascript-tests/week07-day1.js
@@ -0,0 +1,15 @@
+const num =3;
+const numberString = '3';
+const rain = true;
+
+console.log((num, numberString));
+console.log(typeof num, typeof numberString, typeof rain);
+console.log(num + numberString);
+
+if(rain === true) {
+    console.log('go to the gym'); 
+}
+else{
+    console.log('walk');
+}
+
