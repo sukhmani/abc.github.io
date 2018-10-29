56378301ba77a8f32495a47a5addb4cea68b74fb
sukhmani <sukhmani@bellevuecollege.edu>
2018-10-29 12:23:01 -0700

    js

---
+++ JScriptTest/JavaScriptTest/test.js
@@ -0,0 +1,9 @@
+var prog109 = 'Prog109 on Monday';
+
+var a = 10;
+var b = 15;
+
+console.log(prog109);
+console.log(a + b);
+console.log(a*b);
+console.log('a+b =',a +b );
---
+++ JScriptTest/week07-day1.js
@@ -0,0 +1,8 @@
+const num =3;
+const numberString = '3';
+const rain = true;
+
+Console.log((num, numberString));
+console.log(typeof num, typeof numberString, typeof rain);
+console.log(num + numberString);
+
