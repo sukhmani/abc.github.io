e1dc4e8cd6a3fa3b99507c1eb673971fccd154ad
sukhmani <sukhmani@bellevuecollege.edu>
2018-10-31 11:37:59 -0700

    ES6 arrow function

--- javascript-tests/week07-day02.js
+++ javascript-tests/week07-day02.js
@@ -1,3 +1,20 @@
-const result = 1 + 2 * 3;
+var showMessage = (message) =>
+{
+    console.log("=========");
+    console.log("===="+message+"=====");
+    console.log("=========");
+}
 
+showMessage('Operator Precedence');
+let result = 1 + (2 * 3);
 console.log(result);
+
+result = (1 + 2) * 3;
+console.log(result);
+
+ console.log("=========");
+ showMessage('For Loops');
+for(let i = 10; i < 25; i =i+2)
+{
+    console.log(i);
+}
