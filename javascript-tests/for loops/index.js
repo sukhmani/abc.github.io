e29ff1d75d17d4e37dc1de8721cad056ff2f84e1
sukhmani <sukhmani@bellevuecollege.edu>
2018-11-05 10:32:52 -0800

    update

--- javascript-tests/for loops/index.js
+++ javascript-tests/for loops/index.js
@@ -2,6 +2,10 @@ windows.onload =() => {
     const listButton = document.getElementById('run-for-loop');
 
     listButton.onclick =() => {
-        
+        const myList = document.getElementById('myList');
+
+        for (let i; i<s; i++){
+            elfCode.appendToList(myList,i);
+        }
     }
 }
