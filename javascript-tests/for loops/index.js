57353c1b7ec664278de934f83e9fb67c939dded9
sukhmani <sukhmani@bellevuecollege.edu>
2018-11-05 11:48:16 -0800

    done

--- javascript-tests/for loops/index.js
+++ javascript-tests/for loops/index.js
@@ -1,11 +1,27 @@
+
+const addToList = 
+// function(){
+//   //  listButton.onclick =() => {
+//         const myList = document.getElementById('myList');
+
+//         for (let i; i<5; i++){
+//             elfCode.appendToList(myList,i);
+//         }
+//     }
+
 windows.onload =() => {
+
+    /*
+    * listButton: variable
+    * 
+    */
     const listButton = document.getElementById('run-for-loop');
-
-    listButton.onclick =() => {
-        const myList = document.getElementById('myList');
-
-        for (let i; i<s; i++){
-            elfCode.appendToList(myList,i);
-        }
+    listButton.onclick = function(){
+const myList = document.getElementById('myList');
+    
+  //  listButton.onclick = addToList;
+    for (let i; i<5; i++){
+         elfCode.appendToList(myList,i);
     }
+};
 }
