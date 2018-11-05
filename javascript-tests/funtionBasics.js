250e488766bb9805d96467aebc1ff579ad68706a
sukhmani <sukhmani@bellevuecollege.edu>
2018-11-05 12:10:18 -0800

    function arguments and parameters

---
+++ javascript-tests/funtionBasics.js
@@ -0,0 +1,17 @@
+function simpleFunction(){
+    console.log("functions should consist of statements designed to perform a single task.");
+}
+
+function functionParameters(value){
+    console.log(value)
+}
+
+function add(a,b){
+    console.log(a+b);
+}
+function addr(){
+    console.log(2+3);
+}
+simpleFunction();
+functionParameters();
+add(2 + 3);
