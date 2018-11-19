4ad3aaeab95f9b154481edfb411615357819015d
sukhmani <sukhmani@bellevuecollege.edu>
2018-11-19 12:21:32 -0800

    use string for Arrays

--- JavaScriptTest/Week10.html/week10.js
+++ JavaScriptTest/Week10.html/week10.js
@@ -15,12 +15,14 @@ window.onload = () => {
         for (let number of numbers) {
             const li = document.createElement("li");
             li.appendChild(document.createTextNode(number));
-            
+
            // console.log(number);
            numbersDisplay.appendChild(li);
         }
     }
 }
+//const numbers = [3, 2, 1];
+//const string = [one];
 const numbers = [1, 2, 3];
 const objectLiteral = {
     objectName: 'object literal is my name',
