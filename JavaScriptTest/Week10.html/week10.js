275e28fefe15ef87c70652c6906fef2b1e3045d8
sukhmani <sukhmani@bellevuecollege.edu>
2018-11-19 12:16:09 -0800

    create element li> append child

--- JavaScriptTest/Week10.html/week10.js
+++ JavaScriptTest/Week10.html/week10.js
@@ -1,10 +1,31 @@
 window.onload = () => {
     const objectLiteralAction =document.getElementById('object-literal-action')
+    const  numbersAction = document.getElementById('numbers-action');
     objectLiteralAction.onclick = () => {
         const objectLiteralDisplay = document.getElementById('object-literal-display')
-        const displayString = 'you clicked a button';
+        //const displayString = 'you clicked a button';
        // console.log('you clicked the button');
-       console.log(displayString);
-       objectLiteralDisplay.textContent = displayString;
+       console.log(objectLiteral.getName());
+       
+       objectLiteralDisplay.textContent = displayLiteral.getName();
+    }
+    numbersAction.onclick = () => {
+        const numbersDisplay = document.getElementById('numbers-action');
+        numbersDisplay.textContent = numbers;
+        for (let number of numbers) {
+            const li = document.createElement("li");
+            li.appendChild(document.createTextNode(number));
+            
+           // console.log(number);
+           numbersDisplay.appendChild(li);
+        }
+    }
+}
+const numbers = [1, 2, 3];
+const objectLiteral = {
+    objectName: 'object literal is my name',
+
+    getName: function() {
+        return this.objectName;
     }
 }
